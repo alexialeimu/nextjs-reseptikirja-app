5873536c62e88aea336c60f2157835e40aa53efc
alexialeimu <alexialeimu@users.noreply.github.com>
2023-03-07 21:10:26 +0200

    add google authentication

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,17 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 export default function Home() {
-    return <>Hello world</>;
+    const { data } = useSession();
+    return (
+        <div>
+            {data?.user ? (
+                <button onClick={() => signOut()}>Sign Out</button>
+            ) : (
+                <button onClick={() => signIn('google')}>
+                    Sign In
+                </button>
+            )}
+            {data?.user?.name}
+        </div>
+    );
 }
