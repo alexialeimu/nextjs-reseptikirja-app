6ad552aac150d901331c41067fbb0e0f643a20f7
alexialeimu <alexialeimu@users.noreply.github.com>
2023-04-28 23:55:15 +0300

    format instruction text with line breaks + use DOMpurify

--- frontend/src/components/Main/Recipe/RecipeContent.tsx
+++ frontend/src/components/Main/Recipe/RecipeContent.tsx
@@ -11,10 +11,24 @@ const RecipeContent: React.FC<RecipeContentProps> = ({
     recipeData,
     recipeLoading,
 }) => {
+    const instructions = recipeData?.recipe.instructions;
+
+    const formattedInstructions = instructions
+        ? instructions.replace(/\n/g, '<br>')
+        : '';
     console.log('RECIPE DATA', recipeData);
     return (
-        <Stack direction="column" py={2}>
-            <Text>{recipeData?.recipe.instructions}</Text>;
+        <Stack
+            direction="column"
+            py={2}
+            width={{ base: '100%', lg: '60%' }}
+        >
+            <Text
+                dangerouslySetInnerHTML={{
+                    __html: formattedInstructions,
+                }}
+            ></Text>
+            ;
         </Stack>
     );
 };
